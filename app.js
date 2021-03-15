79d8aeff0e60fffae133ffe1fd81f8d054e74c23
William Leung <w.leung.93@gmail.com>
2021-03-14 22:46:31 -0700

    prep for heroku;

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const bodyParser = require('body-parser');
 const passport = require('passport')
 
 
+
+
 mongoose
     .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(()=>console.log("Connected to mongoDB"))
@@ -42,3 +44,16 @@ app.use("/api/tasks", tasks);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)})
+
+
+
+
+//moving to heroku process 
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
